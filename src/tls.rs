ade51a13fdd19d282d85f37f3d06bb1d96ec7dca
Christian Meusel <christian.meusel@posteo.de>
2022-11-06 22:38:11 +0100

    Add PSK support for MQTT

--- src/tls.rs
+++ src/tls.rs
@@ -2,6 +2,55 @@
 use core::ffi::{c_char, CStr};
 use core::fmt::Debug;
 
+use esp_idf_sys::psk_hint_key_t;
+
+use crate::private::cstr::RawCstrs;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Psk<'a> {
+    pub key: &'a [u8],
+    pub hint: &'a str,
+}
+
+impl<'a> Debug for Psk<'a> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
+        f.debug_struct("Psk")
+            .field("hint", &self.hint)
+            .finish_non_exhaustive()
+    }
+}
+
+/// Helper for holding PSK data for lately initialized TLS connections.
+///
+/// It could be easily converted from the public `Psk` configuration and holds the `psk_hint_key_t`
+/// along with its (string) data as this data typically needs to be around after initializing a TLS
+/// client until it has been started.
+pub(crate) struct TlsPsk {
+    pub(crate) psk: Box<psk_hint_key_t>,
+    pub(crate) _cstrs: RawCstrs,
+}
+
+impl Debug for TlsPsk {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
+        f.debug_struct("TlsPsk")
+            .field("psk", &self.psk)
+            .finish_non_exhaustive()
+    }
+}
+
+impl<'a> From<&'a Psk<'a>> for TlsPsk {
+    fn from(conf: &Psk) -> Self {
+        let mut cstrs = RawCstrs::new();
+        let psk = Box::new(psk_hint_key_t {
+            key: conf.key.as_ptr(),
+            key_size: conf.key.len(),
+            hint: cstrs.as_ptr(conf.hint),
+        });
+
+        TlsPsk { psk, _cstrs: cstrs }
+    }
+}
+
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub struct X509<'a>(&'a [u8]);
 
