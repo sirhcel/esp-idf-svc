d0141719ea3fc6cb4d97a75f385b84f359d1ee88
Christian Meusel <christian.meusel@posteo.de>
2022-11-06 22:38:11 +0100

    Add PSK support for MQTT

--- src/mqtt/client.rs
+++ src/mqtt/client.rs
@@ -20,7 +20,7 @@ use crate::private::mutex::RawCondvar;
 pub use asyncify::*;
 
 use crate::private::cstr::*;
-use crate::tls::X509;
+use crate::tls::{Psk, TlsPsk, X509};
 
 pub use client::{Details, MessageId};
 
@@ -80,8 +80,8 @@ pub struct MqttClientConfiguration<'a> {
     pub client_certificate: Option<X509<'static>>,
     pub private_key: Option<X509<'static>>,
     pub private_key_password: Option<&'a str>,
-    // TODO: Future
-    // pub psk_hint_key: KeyHint,
+
+    pub psk: Option<Psk<'a>>,
     // pub alpn_protos: &'a [&'a str],
     // pub use_secure_element: bool,
     // void *ds_data;                          /*!< carrier of handle for digital signature parameters */
@@ -122,12 +122,16 @@ impl<'a> Default for MqttClientConfiguration<'a> {
             client_certificate: None,
             private_key: None,
             private_key_password: None,
+
+            psk: None,
         }
     }
 }
 
 #[cfg(esp_idf_version_major = "4")]
-impl<'a> From<&'a MqttClientConfiguration<'a>> for (esp_mqtt_client_config_t, RawCstrs) {
+impl<'a> From<&'a MqttClientConfiguration<'a>>
+    for (esp_mqtt_client_config_t, RawCstrs, Option<TlsPsk>)
+{
     fn from(conf: &'a MqttClientConfiguration<'a>) -> Self {
         let mut cstrs = RawCstrs::new();
 
@@ -200,7 +204,10 @@ impl<'a> From<&'a MqttClientConfiguration<'a>> for (esp_mqtt_client_config_t, Ra
             }
         }
 
-        (c_conf, cstrs)
+        // FIXME: Support this for IDF 5 too.
+        let tls_psk_conf = conf.psk.as_ref().map(|psk| psk.into());
+
+        (c_conf, cstrs, tls_psk_conf)
     }
 }
 
@@ -330,6 +337,7 @@ pub struct EspMqttClient<S = ()> {
     raw_client: esp_mqtt_client_handle_t,
     conn_state_guard: Option<Arc<ConnStateGuard<RawCondvar, S>>>,
     _boxed_raw_callback: Box<dyn FnMut(esp_mqtt_event_handle_t)>,
+    _tls_psk_conf: Option<TlsPsk>,
 }
 
 impl<S> RawHandle for EspMqttClient<S> {
@@ -437,7 +445,7 @@ impl<S> EspMqttClient<S> {
 
         let unsafe_callback = UnsafeCallback::from(&mut boxed_raw_callback);
 
-        let (mut c_conf, mut cstrs) = conf.into();
+        let (mut c_conf, mut cstrs, tls_psk_conf) = conf.into();
 
         #[cfg(esp_idf_version_major = "4")]
         {
@@ -449,6 +457,11 @@ impl<S> EspMqttClient<S> {
             c_conf.broker.address.uri = cstrs.as_ptr(url);
         }
 
+        // FIXME: Support this for IDF 5 too.
+        if let Some(ref conf) = tls_psk_conf {
+            c_conf.psk_hint_key = &*conf.psk;
+        }
+
         let raw_client = unsafe { esp_mqtt_client_init(&c_conf as *const _) };
         if raw_client.is_null() {
             return Err(EspError::from_infallible::<ESP_FAIL>());
@@ -458,6 +471,7 @@ impl<S> EspMqttClient<S> {
             raw_client,
             _boxed_raw_callback: boxed_raw_callback,
             conn_state_guard,
+            _tls_psk_conf: tls_psk_conf,
         };
 
         esp!(unsafe {
